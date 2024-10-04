b68961514efef5ca0bba3025b3936eec9430e3c0
AbirAppDevs <abir@appdevs.team>
2024-10-04 12:52:59 +0600

    wheel spinner git add

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class SpinnerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Spinner',
       // theme: ThemeData(primarySwatch: Colors.blue),
       home: SpinnerHomePage(),
